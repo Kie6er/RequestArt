0f63d8093e9b187f64dc2fe72c419f62e938f325
Kie6er <kirill.zawgorodny@yandex.ru>
2023-10-23 09:53:38 +0300

    init commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import $ from "jquery";
+
+import Swiper from "swiper";
+
+import "swiper/css";
+import "swiper/css/bundle";
+import "swiper/css/mousewheel";
+import "./js/components/header";
+import "./js/components/footer";
+import "./index.scss";
+
+$(document).ready(function () {
+
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,139 +1,204 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const mode = process.env.NODE_ENV || 'development';
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const path = require("path");
 
-const devMode = mode === 'development';
-const PATHS = {
-	src: path.join(__dirname, './src'),
-	dist: path.join(__dirname, './dist'),
-	assets: 'assets/',
-}
+const mode = process.env.NODE_ENV || "development";
+const devMode = mode === "development";
+const target = devMode ? "web" : "browserslist";
+const devtool = devMode ? "source-map" : undefined;
 
-const target = devMode ? 'web' : 'browserslist';
-const devtool = devMode ? 'source-map' : undefined;
+// Определите точки входа для ваших страниц
+const entryPoints = {
+	index: path.resolve(__dirname, "src", "index.js"),
+	main: path.resolve(__dirname, "src", "index.js"),
+	// food: path.resolve(__dirname, "src", "index.js"),
+	// team: path.resolve(__dirname, "src", "index.js"),
+	// decor: path.resolve(__dirname, "src", "index.js"),
+	// articles: path.resolve(__dirname, "src", "index.js"),
+	// article: path.resolve(__dirname, "src", "index.js"),
+	// costumes: path.resolve(__dirname, "src", "index.js"),
+	// reviews: path.resolve(__dirname, "src", "index.js"),
+	// programs: path.resolve(__dirname, "src", "index.js"),
+	// programs_show: path.resolve(__dirname, "src", "index.js"),
+	// 404: path.resolve(__dirname, 'src', 'index.js'),
+	// contacts: path.resolve(__dirname, 'src', 'index.js'),
+	// detail_show: path.resolve(__dirname, "src", "index.js"),
+	// programs_detail: path.resolve(__dirname, "src", "index.js"),
+	// Добавьте другие страницы здесь
+};
+
+// Создайте экземпляры HtmlWebpackPlugin для каждой страницы
+const htmlPlugins = Object.keys(entryPoints).map((entryName) => {
+	return new HtmlWebpackPlugin({
+		template: path.resolve(__dirname, "src", `${entryName}.html`),
+		filename: `${entryName}.html`, // Имя файла для каждой страницы
+		chunks: "all", // Укажите, какой бандл связать с каждой страницей
+	});
+});
 
 module.exports = {
 	mode,
 	target,
 	devtool,
-	externals: {
-		paths: PATHS
-	},
 	devServer: {
 		port: 3000,
 		open: true,
 		hot: true,
+		watchFiles: ["src/*.html"],
 	},
-	entry: ['@babel/polyfill', path.resolve(__dirname, 'src', 'assets/js/index.js')],
+	//откуда билдить проект
+	entry: {
+		main: path.resolve(__dirname, "src", "index.js"),
+	},
+
 	output: {
-		path: path.resolve(__dirname, 'dist'),
+		//куда выводит билд
+		path: path.resolve(__dirname, "dist"),
+		//очистка билда перед сборкой нового
 		clean: true,
-		filename: 'assets/js/[name].[contenthash].js',
-		assetModuleFilename: 'assets/[name][ext]'
+		//название js файла в билде
+		// [name] - стандартный по вебпаку (main), [contenthash] - добавляептся хэш к названию
+		filename: "[name].js",
+		// assetModuleFilename: 'assets/images',
+	},
+	optimization: {
+		minimizer: [
+			"...",
+			new ImageMinimizerPlugin({
+				minimizer: {
+					implementation: ImageMinimizerPlugin.imageminMinify,
+					options: {
+						plugins: [
+							"imagemin-gifsicle",
+							"imagemin-mozjpeg",
+							"imagemin-pngquant",
+							"imagemin-svgo",
+						],
+					},
+				},
+				generator: [
+					{
+						preset: "webp",
+						implementation: ImageMinimizerPlugin.imageminGenerate,
+						options: {
+							plugins: ["imagemin-webp"],
+						},
+					},
+				],
+			}),
+		],
+		// mangle: false, // Отключение замены имен переменных
 	},
 	plugins: [
-		// Раскоментировать если есть статичные файлы
-		// new CopyWebpackPlugin({
-		// 	patterns: [
-		// 		{ from: `${PATHS.src}/assets/static`, to: `${PATHS.assets}static` },
-		// 	],
+		//сборщик html
+		// new HtmlWebpackPlugin({
+		//   template: path.resolve(__dirname, 'src', 'index.html'),
 		// }),
-		new HtmlWebpackPlugin({
-			template: path.resolve(__dirname, 'src', 'index.html')
-		}),
+		...htmlPlugins,
 		new MiniCssExtractPlugin({
-			filename: 'assets/style.[contenthash].css',
-		})
+			filename: "[name].css",
+		}),
 	],
+
 	module: {
 		rules: [
+			//html
 			{
 				test: /\.html$/i,
-				loader: 'html-loader',
+				loader: "html-loader",
 			},
+			// //изображения js
+			// {
+			//   test: /\.(png|jpe?g|gif)$/i,
+			//   use: [
+			//     {
+			//       loader: 'file-loader',
+			//       options: {
+			//         name: '[path][name].[ext]',
+			//       },
+			//     },
+			//   ],
+			// },
+
+			// css,scss
 			{
-				test: /\.s[ac]ss$/i,
+				test: /\.(c|sa|sc)ss$/i,
 				use: [
-					// Creates `style` nodes from JS strings
 					devMode ? "style-loader" : MiniCssExtractPlugin.loader,
-					// Translates CSS into CommonJS
 					"css-loader",
 					{
-						loader: 'postcss-loader',
+						loader: "postcss-loader",
 						options: {
 							postcssOptions: {
-								plugins: [require('postcss-preset-env')],
-							}
-						}
+								plugins: [require("postcss-preset-env")],
+							},
+						},
 					},
-					// Compiles Sass to CSS
-					"sass-loader",
-				],
-			},
-			{
-				test: /\.(woff2?|ttf|otf|svg|eot)$/i,
-				type: 'asset/resource',
-				generator: {
-					filename: 'assets/fonts/[name][ext]'
-				}
-			},
-			{
-				test: /\.(jpe?g|png|webp|gif|svg)$/i,
-				type: 'asset/resource',
-				use: [
 					{
-						loader: 'image-webpack-loader',
+						loader: "resolve-url-loader",
+					},
+					{
+						loader: "sass-loader",
 						options: {
-							mozjpeg: {
-								progressive: true,
-							},
-							// optipng.enabled: false will disable optipng
-							optipng: {
-								enabled: false,
-							},
-							pngquant: {
-								quality: [0.65, 0.90],
-								speed: 4
-							},
-							gifsicle: {
-								interlaced: false,
-							},
-							// the webp option will enable WEBP
-							webp: {
-								quality: 75
-							}
-						}
-					}
+							sourceMap: true,
+						},
+					},
 				],
-				generator: {
-					filename: 'assets/images/[hash][ext]'
-				}
 			},
+			// шрифты
+			{
+				test: /\.(woff|ttf|otf)$/i,
+				type: "asset/resource",
+				generator: {
+					filename: "assets/fonts/[name][ext]",
+				},
+			},
+			// изображения
 			{
 				test: /\.(jpe?g|png|webp|gif|svg)$/i,
-				type: 'asset/resource',
+				use: devMode
+					? []
+					: [
+						{
+							loader: "image-webpack-loader",
+							options: {
+								mozjpeg: {
+									progressive: true,
+								},
+								optipng: {
+									enabled: false,
+								},
+								pngquant: {
+									quality: [0.65, 0.9],
+									speed: 4,
+								},
+								gifsicle: {
+									interlaced: false,
+								},
+								webp: {
+									quality: 75,
+								},
+							},
+						},
+					],
+				type: "asset/resource",
 				generator: {
-					filename: 'assets/images/icons/[hash][ext]'
-				}
+					filename: "assets/images/[name][ext]",
+				},
 			},
+
+			//js
 			{
-				test: /\.(?:js|mjs|cjs)$/,
+				test: /\.(?:js|mjs|cjs)$/i,
 				exclude: /node_modules/,
 				use: {
-					loader: 'babel-loader',
+					loader: "babel-loader",
 					options: {
-						presets: [
-							['@babel/preset-env', {
-								targets: "defaults",
-								useBuiltIns: "entry",
-								"corejs": 3
-							}]
-						]
-					}
-				}
-			}
-		]
-	}
-}
+						presets: [["@babel/preset-env", { targets: "defaults" }]],
+					},
+				},
+			},
+		],
+	},
+};
