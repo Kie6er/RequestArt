f8ae4818f45c55be2ab68ea14608f4ba33ea67ea
Kie6er <kirill.zawgorodny@yandex.ru>
2023-11-09 17:54:14 +0300

    add blocks

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import $ from "jquery";
 
-import Swiper from "swiper";
-
 import "swiper/css";
 import "swiper/css/bundle";
 import "swiper/css/mousewheel";
 import "./js/components/header";
 import "./js/components/footer";
+import "./js/components/swiper";
+import "./js/pages/main";
 import "./index.scss";
 
 $(document).ready(function () {
